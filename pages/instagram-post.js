a95059840d6bc576d106390f2b3550c13117213d
teishito <shito.tei@foru.co.jp>
2025-11-04 11:27:56 +0900

    Update and rename campaign-detail.js to instagram-post.js

--- pages/instagram-post.js
+++ pages/instagram-post.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import CampaignDetail from "../components/CampaignDetail";
+import CampaignDetail from "../components/InstagramPost";
 
 export default function CampaignDetailPage() {
   return (
