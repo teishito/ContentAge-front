7e40acf0468942044874a5be2550d468917f8625
teishito <shito.tei@foru.co.jp>
2025-11-06 14:41:40 +0900

    Update instagram-post.js

--- pages/instagram-post.js
+++ pages/instagram-post.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import InstagramPost from "../components/InstagramPost";
 
-export default function CampaignDetailPage() {
+export default function InstagramPostPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
