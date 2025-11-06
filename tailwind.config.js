584cb23e7acfdcb427d073f97f3d7078d7b49b3f
teishito <shito.tei@foru.co.jp>
2025-11-06 14:19:55 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         gray2: "#E5E5E5",
         orange: "#F48B00",
         blue: "#004EFF",
-        primary: "#5B7F6F", // ContentAgeテーマカラー
+        primary: "#5B7F6F",
       },
       fontFamily: {
         sans: ["'Zen Kaku Gothic New'", "'Urbanist'", "sans-serif"],
@@ -21,9 +21,7 @@ module.exports = {
       boxShadow: {
         card: "0 4px 20px rgba(0,0,0,0.05)",
       },
-      borderRadius: {
-        lg: "0.75rem",
-      },
+      borderRadius: { lg: "0.75rem" },
     },
   },
   plugins: [],
