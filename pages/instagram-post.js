9b0388a923c3b58d3204db408568215ac707e33e
teishito <shito.tei@foru.co.jp>
2025-11-06 14:41:20 +0900

    Update instagram-post.js

--- pages/instagram-post.js
+++ pages/instagram-post.js
@@ -1,13 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import CampaignDetail from "../components/InstagramPost";
+import InstagramPost from "../components/InstagramPost";
 
 export default function CampaignDetailPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
       <main className="flex-grow flex flex-col items-center p-4 pb-40">
-        <CampaignDetail />
+        <InstagramPost />
       </main>
       <Footer />
     </div>
