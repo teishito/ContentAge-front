ef8ec6ad90ccf0326cb15cb97fbbcdeec29ca895
teishito <shito.tei@foru.co.jp>
2025-12-05 12:24:04 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://contentage-back.azurewebsites.net/api/:path*', // バックエンドのURL
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
